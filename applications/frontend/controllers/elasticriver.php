adac63aa6730b962dfb23ba0d6f491acb67fc137
Joohelmer <joo.helmer@gmail.com>
2015-11-16 16:30:16 +0100

    Ajout de l'elastic search

---
+++ applications/frontend/controllers/elasticriver.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Elasticriver extends CI_Controller {
+
+    public $globalVar = array();
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library("session");
+        $this->load->library("elasticsearch");
+    }
+
+    public function getRestaurants(){
+
+		$restos = $this->db->query('SELECT * FROM restaurants' )->result(); 
+        
+        foreach ($restos as $value) {
+            $id = $value->id;
+            $data = array("titre"=>$value->titre, "description"=>$value->description);
+            $this->elasticsearch->add("restaurants", $id, $data);
+        }
+
+        echo "Terminé.";
+    }
+
+    public function getEvents(){
+
+		$events = $this->db->query('SELECT * FROM evenements' )->result(); 
+        
+        foreach ($events as $value) {
+            $id = $value->id;
+            $data = array("titre"=>$value->titre, "description"=>$value->description);
+            $this->elasticsearch->add("restaurants", $id, $data);
+        }
+
+        echo "Terminé.";
+    }
+ 
+
+
+}
